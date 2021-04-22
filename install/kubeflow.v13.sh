da4c9ee41d9973d1b679a2d117f6868bbb380fdf
hsinhoyeh <yhh92u@gmail.com>
2021-04-22 12:54:23 +0800

    use kubeflow 1.3 rc1

--- install/kubeflow.v13.sh
+++ install/kubeflow.v13.sh
@@ -6,7 +6,7 @@ apt-get install -y wget
 
 # use 3.2.x for install kubeflow
 wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.2.3/kustomize_kustomize.v3.2.3_linux_amd64
-chmod +X ./kustomize_kustomize.v3.2.3_linux_amd64 && \
+chmod +x ./kustomize_kustomize.v3.2.3_linux_amd64 && \
   mv ./kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
 
 KUBEFLOW_ROOT=/root/kubeflow/v1.3
@@ -16,8 +16,8 @@ KF_DIR=${KUBEFLOW_ROOT}/${KF_NAME}
 mkdir -p $KF_DIR
 
 cd $KF_DIR && \
-  wget https://github.com/kubeflow/manifests/archive/refs/tags/v1.3.0-rc.0.tar.gz && \
-  tar -xzvf v1.3.0-rc.0.tar.gz && \
-  cd manifests-1.3.0-rc.0
+  wget https://github.com/kubeflow/manifests/archive/refs/tags/v1.3.0-rc.1.tar.gz && \
+  tar -xzvf v1.3.0-rc.1.tar.gz && \
+  cd manifests-1.3.0-rc.1
 
 while ! kustomize build --load_restrictor=none example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
