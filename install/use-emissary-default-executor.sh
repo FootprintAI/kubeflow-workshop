ef87239fddc6aa54a416084df3f11f87dc9aca14
hsinhoyeh <yhh92u@gmail.com>
2021-11-29 15:13:06 +0800

    fix: typo

--- install/use-emissary-default-executor.sh
+++ install/use-emissary-default-executor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl patch configmap workflow-controller-configmap --patch {\"data\":{\"containerRuntimeExecutor\":\"emissary\"}}" -n kubeflow
+kubectl patch configmap workflow-controller-configmap --patch {\"data\":{\"containerRuntimeExecutor\":\"emissary\"}} -n kubeflow
 kubectl rollout restart deployment/workflow-controller -n kubeflow
